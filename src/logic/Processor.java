659bafd4939e8f1948e21a8d99b5b14b2d8589c1
larry852 <larryportocarrero@gmail.com>
2016-11-30 10:44:59 -0500

    Detencion del hilo y reinicio de los componentes al finalizar el proceso

--- src/logic/Processor.java
+++ src/logic/Processor.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import gui.MainWindows;
 import persistence.Memory;
 import persistence.Step;
+import sun.management.resources.agent_it;
 
 public class Processor {
 
@@ -17,15 +18,16 @@ public class Processor {
 				ArrayList<Step> steps = memory.readProcess(process);
 				for (int i = 0; i < steps.size(); i++) {
 					MainWindows.getInstancia().drawComponents(steps.get(i).getComponet());
-
-					MainWindows.getInstancia().drawInstructions(steps.get(i).getInstruction() + " (" + process.charAt(process.length()-1) + ")");
-
+					MainWindows.getInstancia().drawInstructions(
+							steps.get(i).getInstruction() + " (" + process.charAt(process.length() - 1) + ")");
 					try {
 						Thread.sleep(5000);
 					} catch (InterruptedException ex) {
 						Thread.currentThread().interrupt();
 					}
 				}
+				MainWindows.getInstancia().Again();
+				thread.interrupt();
 			}
 		};
 		thread.start();
